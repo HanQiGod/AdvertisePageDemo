679a90ea8dd470ffa3d97b4e7112439e0d25d0f2
HanQiGod <1539841933@qq.com>
2018-10-09 15:05:13 +0800

    second commit

--- AdvertisePageDemo/ViewController.m
+++ AdvertisePageDemo/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "AdvertiseViewController.h"
 
 @interface ViewController ()
 
@@ -16,8 +17,19 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.title = @"首页";
+    
+    self.view.backgroundColor = [UIColor orangeColor];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(pushToAd) name:@"pushtoad" object:nil];
 }
 
+- (void)pushToAd {
+    
+    AdvertiseViewController *adVc = [[AdvertiseViewController alloc] init];
+    [self.navigationController pushViewController:adVc animated:YES];
+    
+}
 
 @end
